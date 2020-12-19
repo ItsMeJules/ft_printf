5947404448f9a279e67e6386c5776892de51a648
Jules <jules.prn06@gmail.com>
2020-12-19 17:30:01 +0100

    yet again another fix

--- srcs/converters/converters_utils.c
+++ srcs/converters/converters_utils.c
@@ -6,7 +6,7 @@
 /*   By: jules <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/07 21:23:10 by jules             #+#    #+#             */
-/*   Updated: 2020/12/19 17:14:43 by jules            ###   ########.fr       */
+/*   Updated: 2020/12/19 17:29:46 by jules            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,14 +33,14 @@ void	precision_print(t_helper *helper)
 void	prec_pad_checks(t_helper *helper, int val, int digits)
 {
 	if (helper->precision > 0)
-		helper->pad_len -= helper->precision + (val < 0 ? 1 : 0);
+		helper->pad_len -= helper->precision + (val < 0 ? 2 : 1);
 	else
 	{
 		if (val < 0)
 			helper->pad_len -= digits + 1;
 		else if (val > 0)
 			helper->pad_len -= digits;
-		else
+		else if (helper->precision != 0)
 			helper->pad_len--;
 	}
 	helper->precision -= digits;
